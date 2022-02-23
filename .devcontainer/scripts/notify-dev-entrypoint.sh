293eeb9fd20e46d78f171ff23bb58038dee0b29f
Pat Heard <patrick.heard@cds-snc.ca>
2022-02-23 17:23:59 -0500

    fix: update the devcontainer with k8s tools (#937)
    Add kubectl, kubens, AWS cli and aliases to make manifest
    support and development easier.

--- .devcontainer/scripts/notify-dev-entrypoint.sh
+++ .devcontainer/scripts/notify-dev-entrypoint.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
-set -ex 
+set -ex
 
 ###################################################################
-# This script will get executed *once* the Docker container has 
+# This script will get executed *once* the Docker container has
 # been built. Commands that need to be executed with all available
-# tools and the filesystem mount enabled should be located here. 
+# tools and the filesystem mount enabled should be located here.
 ###################################################################
 
 # Define aliases
-echo -e "\n\n# User's Aliases" >> ~/.profile
-echo -e "alias fd=fdfind" >> ~/.profile
-echo -e "alias l='ls -al --color'" >> ~/.profile
-echo -e "alias ls='exa'" >> ~/.profile
-echo -e "alias l='exa -alh'" >> ~/.profile
-echo -e "alias ll='exa -alh@ --git'" >> ~/.profile
-echo -e "alias lt='exa -al -T -L 2'" >> ~/.profile
+echo -e "\n\n# User's Aliases" >> ~/.zshrc
+echo -e "alias fd=fdfind" >> ~/.zshrc
+echo -e "alias l='ls -al --color'" >> ~/.zshrc
+echo -e "alias ls='exa'" >> ~/.zshrc
+echo -e "alias l='exa -alh'" >> ~/.zshrc
+echo -e "alias ll='exa -alh@ --git'" >> ~/.zshrc
+echo -e "alias lt='exa -al -T -L 2'" >> ~/.zshrc
 
-# Warm up git index 
+# AWS cli autocomplete and aliases
+echo -e "complete -C /usr/local/bin/aws_completer aws" >> ~/.zshrc
+
+# Kubectl aliases and command autocomplete
+echo -e "alias k='kubectl'" >> ~/.zshrc
+echo -e "alias k-staging='aws eks --region ca-central-1 update-kubeconfig --name notification-canada-ca-staging-eks-cluster'" >> ~/.zshrc
+echo -e "alias k-prod='aws eks --region ca-central-1 update-kubeconfig --name notification-canada-ca-production-eks-cluster'" >> ~/.zshrc
+echo -e "source <(kubectl completion zsh)" >> ~/.zshrc
+echo -e "complete -F __start_kubectl k" >> ~/.zshrc
+
+cd /workspaces/notification-manifests
+
+# Warm up git index prior to display status in prompt
 git status
