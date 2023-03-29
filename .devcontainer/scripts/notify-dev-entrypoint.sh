5460db4170e8e8f3a0d6c8499a85afd1274a3b8e
Steve Astels <sastels@users.noreply.github.com>
2023-03-29 17:02:38 -0400

    chore: add more k8s setup to devcontainer (#1497)

--- .devcontainer/scripts/notify-dev-entrypoint.sh
+++ .devcontainer/scripts/notify-dev-entrypoint.sh
@@ -17,9 +17,13 @@ echo -e "alias ll='exa -alh@ --git'" >> ~/.zshrc
 echo -e "alias lt='exa -al -T -L 2'" >> ~/.zshrc
 
 # AWS cli autocomplete and aliases
+echo -e "alias sso-staging='aws sso login --profile notify-staging'" >> ~/.zshrc
+echo -e "alias sso-prod='aws sso login --profile notify-prod'" >> ~/.zshrc
 echo -e "complete -C /usr/local/bin/aws_completer aws" >> ~/.zshrc
 
 # Kubectl aliases and command autocomplete
+echo -e "alias staging='export AWS_PROFILE=notify-staging && kubectl config use-context notify-staging'" >> ~/.zshrc
+echo -e "alias prod='export AWS_PROFILE=notify-prod && kubectl config use-context notify-prod'" >> ~/.zshrc
 echo -e "alias k='kubectl'" >> ~/.zshrc
 echo -e "alias k-staging='aws eks --region ca-central-1 update-kubeconfig --name notification-canada-ca-staging-eks-cluster'" >> ~/.zshrc
 echo -e "alias k-prod='aws eks --region ca-central-1 update-kubeconfig --name notification-canada-ca-production-eks-cluster'" >> ~/.zshrc
@@ -27,6 +31,9 @@ echo -e "source <(kubectl completion zsh)" >> ~/.zshrc
 echo -e "complete -F __start_kubectl k" >> ~/.zshrc
 
 cd /workspaces/notification-manifests
+
+# kubent
+ sh -c "$(curl -sSL https://git.io/install-kubent)"
 
 # Warm up git index prior to display status in prompt
 git status
