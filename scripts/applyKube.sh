74d04ba75f6670d4781b829525637d390b3d512b
Ben Larabie <ben.larabie@cds-snc.ca>
2023-09-20 10:46:27 -0400

    Adding the bash header and a quick description of kube apply script

--- scripts/applyKube.sh
+++ scripts/applyKube.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+### This script will attempt to apply kustomize for a given environment, and retry up to 5 times on failure.
+### This is primarily required for fresh environments and new app installs that have CRDs and Webhooks.
+### Kustomize does not have a mechanism to wait for resources to become "ready" before proceeding
+
 retryCount=0
 workDir=$1
 kubeConfig=$2
