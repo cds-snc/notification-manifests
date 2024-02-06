0aed9160e3c060988b5a3f3acb0ca1805da519e5
Ben Larabie <ben.larabie@cds-snc.ca>
2024-02-06 15:43:05 -0500

    Reconciling nginx to one chart

--- helmfile/getContext.sh
+++ helmfile/getContext.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+###################################################################################################
+# This script is used to load the necessary environment variables to run helmfile                 #
+# The single argument passed is to identify that this is being run from github as an action.      #
+# The argument can be literally anything, as the script just checks that it exists                #
+#                                                                                                 #
+# If running locally, no arguments are required. Simply run:                                      #
+#                                                                                                 #
+# source ./getContext.sh                                                                          #
+#                                                                                                 #
+###################################################################################################
+
+
 GITHUB=$1
 getValue()
 {
