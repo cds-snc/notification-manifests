13b8c0140bf3d26a5ebd7ecb1deb97ca79c43105
Ben Larabie <ben.larabie@cds-snc.ca>
2024-09-12 09:35:35 -0400

    Moving create vpn config to script

---
+++ scripts/createVPNConfig.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# This script will create a VPN configuration file for the specified environment
+# Usage: ./createVPNConfig.sh <environment>
+# Example: ./createVPNConfig.sh staging
+ENVIRONMENT=$1
+git clone https://github.com/cds-snc/notification-terraform.git /var/tmp/notification-terraform
+cd /var/tmp/notification-terraform/env/$ENVIRONMENT/eks
+ENDPOINT_ID=$(terragrunt output --raw gha_vpn_id)
+CERT=$(terragrunt output --raw gha_vpn_certificate)
+KEY=$(terragrunt output --raw gha_vpn_key)
+aws ec2 export-client-vpn-client-configuration --client-vpn-endpoint-id $ENDPOINT_ID --output text > /var/tmp/$ENVIRONMENT.ovpn
+echo "<cert>
+$CERT
+</cert>" >> /var/tmp/$ENVIRONMENT.ovpn
+echo "<key>
+$KEY
+</key>" >> /var/tmp/$ENVIRONMENT.ovpn
