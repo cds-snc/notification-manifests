91f90ced294d78f8cd1f431626838fdc5a36d28b
Ben Larabie <ben.larabie@cds-snc.ca>
2024-10-03 14:10:16 -0400

    yep

--- scripts/createVPNConfig.sh
+++ scripts/createVPNConfig.sh
@@ -10,7 +10,7 @@ else
 fi
 echo $VAULT
 git clone https://github.com/cds-snc/notification-terraform.git /var/tmp/notification-terraform
-op read op://4eyyuwddp6w4vxlabrr2i2duxm/"TFVars - $ENVIRONMENT"/notesPlain > /var/tmp/notification-terraform/aws/$ENVIRONMENT.tfvars   
+op read op://$VAULT/"TFVars - $ENVIRONMENT"/notesPlain > /var/tmp/notification-terraform/aws/$ENVIRONMENT.tfvars   
 cd /var/tmp/notification-terraform/env/$ENVIRONMENT/eks
 export INFRASTRUCTURE_VERSION=$(cat ../../../.github/workflows/infrastructure_version.txt)
 ENDPOINT_ID=$(terragrunt output --raw gha_vpn_id)
