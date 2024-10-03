37bb7f1b901c238d158dc65db48d50731cc902e2
Ben Larabie <ben.larabie@cds-snc.ca>
2024-10-03 14:22:46 -0400

    [review] vault

--- scripts/createVPNConfig.sh
+++ scripts/createVPNConfig.sh
@@ -8,7 +8,6 @@ if [ "$ENVIRONMENT" == "production" ]; then
 else
   VAULT=4eyyuwddp6w4vxlabrr2i2duxm
 fi
-echo $VAULT
 git clone https://github.com/cds-snc/notification-terraform.git /var/tmp/notification-terraform
 op read op://$VAULT/"TFVars - $ENVIRONMENT"/notesPlain > /var/tmp/notification-terraform/aws/$ENVIRONMENT.tfvars   
 cd /var/tmp/notification-terraform/env/$ENVIRONMENT/eks
