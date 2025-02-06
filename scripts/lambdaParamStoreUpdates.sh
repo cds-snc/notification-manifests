b36bb6268a416e9e0792b1bca7387753fb35db83
Mike Pond <32133001+P0NDER0SA@users.noreply.github.com>
2025-02-06 10:05:57 -0500

    Separating Admin and API Param stores (#3518)
    * separating these env vars to separate param stores
    
    * dummy updartes

--- scripts/lambdaParamStoreUpdates.sh
+++ scripts/lambdaParamStoreUpdates.sh
@@ -14,9 +14,6 @@ params="$params\n$secrets"
 params=""
 # API
 get_env_and_secrets "notify-api"
-# ADMIN
-get_env_and_secrets "notify-admin"
-
 params=$(echo -e "$params" | sort -u)
 aws ssm get-parameters --region ca-central-1 --with-decryption --names ENVIRONMENT_VARIABLES --query 'Parameters[*].Value' --output text > .previous.env
 aws ssm put-parameter --region ca-central-1 --name ENVIRONMENT_VARIABLES --type SecureString --key-id alias/aws/ssm --value "$params" --tier "Intelligent-Tiering" --overwrite
@@ -24,3 +21,8 @@ aws ssm get-parameters --region ca-central-1 --with-decryption --names ENVIRONME
 DIFF=$(diff -B .new.env .previous.env | wc -l | tr -d ' ')
 echo "DIFF=$DIFF"
 export DIFF
+params=""
+# ADMIN
+get_env_and_secrets "notify-admin"
+params=$(echo -e "$params" | sort -u)
+aws ssm put-parameter --region ca-central-1 --name ENVIRONMENT_VARIABLES_ADMIN --type SecureString --key-id alias/aws/ssm --value "$params" --tier "Intelligent-Tiering" --overwrite
