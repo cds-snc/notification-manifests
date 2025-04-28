aaff26379af56f406fcbc96fda101d875e15d92f
Ben Larabie <ben.larabie@blarabie-jtfk.home>
2025-04-28 14:54:13 -0400

    script comments

--- scripts/karpenterUpgrade.sh
+++ scripts/karpenterUpgrade.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+
+# This script is used to upgrade Karpenter when there are breaking changes
+# that prevent a smooth upgrade with Helmfile.
+# This should only be used if you can not smoothly upgrade with Helmfile.
+# It will delete all the existing CRDs and Karpenter resources
+# and then reapply the new CRDs and resources
+
+ENVIRONMENT=$1
+if [ -z "$ENVIRONMENT" ]; then
+    echo "Usage: $0 <environment>"
+    exit 1
+fi
+
 EC2_NODE_CLASSES=$(kubectl get ec2nodeclasses.karpenter.k8s.aws | awk '{print $1}' | tail -n +2)
 
 for NODE_CLASS in $EC2_NODE_CLASSES; do
@@ -24,9 +37,9 @@ kubectl delete crd $(kubectl get crd -n karpenter | grep karpenter | awk '{print
 
 pushd ../helmfile
 source getContext.sh
-helmfile -e staging -l app=karpenter,tier=crd sync
+helmfile -e $ENVIRONMENT -l app=karpenter,tier=crd sync
 sleep 5
-helmfile -e staging -l app=karpenter,tier=main apply
+helmfile -e $ENVIRONMENT -l app=karpenter,tier=main apply
 sleep 5
-helmfile -e staging -l app=karpenter,tier=nodepool apply
+helmfile -e $ENVIRONMENT -l app=karpenter,tier=nodepool apply
 popd
