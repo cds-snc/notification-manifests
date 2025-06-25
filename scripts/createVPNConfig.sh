2645b2376587614cd168e2d569c8a90f07868a05
Ben Larabie <ben.larabie@blarabie-jtfk.home>
2025-06-25 10:44:49 -0400

    signoz

--- scripts/createVPNConfig.sh
+++ scripts/createVPNConfig.sh
@@ -8,17 +8,18 @@ if [ "$ENVIRONMENT" == "production" ]; then
 else
   VAULT=4eyyuwddp6w4vxlabrr2i2duxm
 fi
-git clone https://github.com/cds-snc/notification-terraform.git /var/tmp/notification-terraform
-op read op://$VAULT/"TERRAFORM_SECRETS_$ENVIRONMENT"/notesPlain > /var/tmp/notification-terraform/aws/$ENVIRONMENT.tfvars   
-cd /var/tmp/notification-terraform/env/$ENVIRONMENT/eks
-export INFRASTRUCTURE_VERSION=$(cat ../../../.github/workflows/infrastructure_version.txt)
+#git clone https://github.com/cds-snc/notification-terraform.git /var/tmp/notification-terraform
+#op read op://$VAULT/"TERRAFORM_SECRETS_$ENVIRONMENT"/notesPlain > /var/tmp/notification-terraform/aws/$ENVIRONMENT.tfvars   
+#cd /var/tmp/notification-terraform/env/$ENVIRONMENT/eks
+cd ~/projects/notification-terraform/env/$ENVIRONMENT/eks
+#export INFRASTRUCTURE_VERSION=$(cat ../../../.github/workflows/infrastructure_version.txt)
 ENDPOINT_ID=$(terragrunt output --raw gha_vpn_id)
 CERT=$(terragrunt output --raw gha_vpn_certificate)
 KEY=$(terragrunt output --raw gha_vpn_key)
-aws ec2 export-client-vpn-client-configuration --client-vpn-endpoint-id $ENDPOINT_ID --output text > /var/tmp/$ENVIRONMENT.ovpn
+aws ec2 export-client-vpn-client-configuration --client-vpn-endpoint-id $ENDPOINT_ID --output text > ~/projects/temp/$ENVIRONMENT.ovpn
 echo "<cert>
 $CERT
-</cert>" >> /var/tmp/$ENVIRONMENT.ovpn
+</cert>" >> ~/projects/temp/$ENVIRONMENT.ovpn
 echo "<key>
 $KEY
-</key>" >> /var/tmp/$ENVIRONMENT.ovpn
+</key>" >> ~/projects/temp/$ENVIRONMENT.ovpn
